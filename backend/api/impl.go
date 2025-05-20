8ee7b81ebaac592665ada38c4fdbec45ca431e8c
Tima-Zan <tzaneuski@gmail.com>
2025-05-20 09:23:49 +0300

    The returned object has been changed to confirm-email

--- backend/api/impl.go
+++ backend/api/impl.go
@@ -13,16 +13,26 @@ type Server struct {
 	db      *db.Queries
 }
 
-// GetCurrentUser implements ServerInterface.
-func (s *Server) GetCurrentUser(w http.ResponseWriter, r *http.Request) {
-	panic("unimplemented")
-}
-
-// Login implements ServerInterface.
-func (s *Server) Login(w http.ResponseWriter, r *http.Request) {
-	panic("unimplemented")
-}
-
 func NewServer(jwtAuth tools.Authenticator, db *db.Queries) Server {
 	return Server{jwtAuth: jwtAuth, db: db}
+}
+
+func (s *Server) ConfirmEmail(w http.ResponseWriter, r *http.Request) {
+	panic("ConfirmEmail endpoint not implemented yet.")
+}
+
+func (s *Server) Login(w http.ResponseWriter, r *http.Request) {
+	panic("Login endpoint not implemented yet.")
+}
+
+func (s *Server) Logout(w http.ResponseWriter, r *http.Request) {
+	panic("Logout endpoint not implemented yet.")
+}
+
+func (s *Server) Register(w http.ResponseWriter, r *http.Request) {
+	panic("Register endpoint not implemented yet.")
+}
+
+func (s *Server) ValidateToken(w http.ResponseWriter, r *http.Request) {
+	panic("Validate token endpoint not implemented yet.")
 }
